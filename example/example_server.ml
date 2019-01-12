9bbb445cfc17ba356873ebcebad25de456e82e3f
Dorian <dorian@kudi.ai>
2019-01-12 13:44:18 +0000

    Move websockets to own repository
    Package renaming
    
    Update repo url
    
    Allow distinction between client and server handling
    
    Add write_frame function
    
    Fix compilation issues with weak type variables
    
    Allow sending and receiving of websockets
    
    Add ping-pong response
    
    Remove dependency on lwt
    
    Respond with pong conaining ping application data (5.5.3)
    
    Close code is 2-byte integer (5.5.1)
    
    Handle close frames appropriately (7.1.1)
    
    Update comments
    
    Add example, remove merlin files

---
+++ example/example_server.ml
@@ -0,0 +1,38 @@
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+module Websocket = Ws.Make(Interface'_lwt.Io)
+
+let ws_handler send =
+  Some "Welcome to my websocket!" |> send
+  >>= fun _ ->
+  return (function
+    | Some m -> Lwt_io.printf "Received message: %s\n" m
+      >>= fun _ -> Some (Printf.sprintf "Thanks, I got [%s]" m) |> send
+    | None -> Lwt_io.printf "Connection closed\n")
+
+let server =
+  let callback _conn req _body =
+    let meth = req |> Request.meth in
+    let headers = req |> Request.headers |> Header.to_list in
+    match meth with
+      | `GET ->
+          (if Ws.is_websocket_upgrade headers then
+            match Websocket.upgrade headers with
+              | Error e_headers ->
+                let res = Response.make ~status:`Bad_request ~headers:(e_headers |> Header.of_list) () in
+                (res, fun _ oc -> Lwt_io.close oc) |> return
+              | Ok ok_headers ->
+                let res = Response.make ~status:`Switching_protocols ~headers:(ok_headers |> Header.of_list) () in
+                (res, fun ic oc -> Websocket.handle_server ws_handler ic oc) |> return
+          else
+            let res = Response.make ~status:`Bad_request () in
+            (res, fun _ oc -> Lwt_io.close oc) |> return)
+      | _ ->
+        let res = Response.make ~status:`Method_not_allowed () in
+          (res, fun _ oc -> Lwt_io.close oc) |> return
+  in
+    Server.create ~mode:(`TCP (`Port 8000)) (Server.make_expert ~callback ())
+
+let () = ignore (Lwt_main.run server)
